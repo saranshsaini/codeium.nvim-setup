06f6d4b20a47efab85b2348c73c6811d0e42ac19
Saransh Saini <saranshs@berkeley.edu>
2024-11-01 14:56:22 -0400

    basic setup

--- lua/plugins/codeium.lua
+++ lua/plugins/codeium.lua
@@ -2,7 +2,7 @@ return {
     "Exafunction/codeium.nvim",
     dependencies = {
         "nvim-lua/plenary.nvim",
-        -- "hrsh7th/nvim-cmp",
+        "hrsh7th/nvim-cmp",
     },
     config = function()
         require("codeium").setup({
